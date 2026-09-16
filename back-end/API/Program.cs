a0a1bd482d09731320d8725e9032b96ee6fb674f
Adam Pertek <adam.a.pertek@gmail.com>
2026-09-16 23:01:58 +0200

    Install `scalar`

--- back-end/API/Program.cs
+++ back-end/API/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.OpenApi;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+	// https://localhost:7221/openapi/v1.json
+	options.OpenApiVersion = OpenApiSpecVersion.OpenApi3_0;
+});
 
 var app = builder.Build();
 
@@ -12,6 +18,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
+	// https://localhost:7221/scalar
+	app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
